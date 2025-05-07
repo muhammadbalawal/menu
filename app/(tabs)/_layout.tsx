3f8994e89890e42ea70559210079430eed78a5c0
muhammadbalawal <2230641@johnabbottcollege.net>
2025-05-07 00:30:27 +0000

    added camera

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
+          title: 'Camera',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
